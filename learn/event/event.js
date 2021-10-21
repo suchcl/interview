c2778fff0f54e3e5937b6baa18256671ea9c9b62
yanshijing <yanshijing@comjia.com>
2021-10-21 18:32:01 +0800

    案例

--- learn/event/event.js
+++ learn/event/event.js
@@ -268,6 +268,7 @@
 //   await async2();
 //   console.log("async1 end");
 // }
+
 // async function async2() {
 //   new Promise(function (resolve) {
 //     console.log("promise1");
@@ -277,10 +278,13 @@
 //   });
 // }
 // console.log("script start");
+
+// // 第一个宏任务
 // setTimeout(function () {
 //   console.log("setTimeout");
 // }, 0);
 // async1();
+
 // new Promise(function (resolve) {
 //   console.log("promise3");
 //   resolve();
@@ -291,34 +295,97 @@
 // script start、async1 start、promise1、promise3、script end、promise2、async1 end、promise4、setTimeout
 
 // 13
-async function async1() {
-  console.log("async1 start");
-  await async2();
-  setTimeout(function () {
-    console.log("settimeout1");
-  }, 0);
-}
+// async function async1() {
+//   console.log("async1 start");
+//   await async2();
+//   setTimeout(function () {
+//     console.log("settimeout1");
+//   }, 0);
+// }
 
-async function async2() {
-  setTimeout(function () {
-    console.log("settimeout2");
-  }, 0);
-}
+// async function async2() {
+//   setTimeout(function () {
+//     console.log("settimeout2");
+//   }, 0);
+// }
 
-console.log("script start");
+// console.log("script start");
 
-setTimeout(function () {
-  console.log("setTimeout3");
-}, 0);
+// setTimeout(function () {
+//   console.log("setTimeout3");
+// }, 0);
 
-async1();
+// async1();
 
-new Promise(function (resolve) {
-  console.log("promise1");
-  resolve();
-}).then(function () {
-  console.log("promise2");
-});
+// new Promise(function (resolve) {
+//   console.log("promise1");
+//   resolve();
+// }).then(function () {
+//   console.log("promise2");
+// });
 
-console.log("script end");
+// console.log("script end");
 //script start、async1 start、promise1、script end、promise2、settimeout3、settimeout2、settimeout1
+
+// 14
+// async function async1() {
+//   console.log("async1 start");
+//   await async2();
+//   console.log("async1 end");
+// }
+
+// async function async1() {
+//   console.log("async1 start");
+//   Promise.resolve(async2()).then(function () {
+//     console.log("async1 end");
+//   });
+// }
+
+// 15
+// async function a1() {
+//   console.log("a1 start");
+//   await a2();
+//   console.log("a1 end");
+// }
+
+// // async function a1() {
+// //   console.log("a1 start");
+// //   Promise.resolve(a2()).then(() => {
+// //     console.log("a1 end");
+// //   });
+// // }
+
+// async function a2() {
+//   console.log("a2");
+// }
+
+// console.log("script start");
+
+// // 第一个宏任务，被挂起
+// setTimeout(() => {
+//   console.log("setTimeout");
+// }, 0);
+
+// Promise.resolve().then(() => {
+//   console.log("promise1");
+// });
+
+// a1();
+
+// let promise2 = new Promise((resolve) => {
+//   resolve("promise2.then");
+//   console.log("promise2");
+// });
+
+// promise2.then((res) => {
+//   console.log(res);
+//   Promise.resolve().then(() => {
+//     console.log("promise3");
+//     setTimeout(function () {
+//       console.log("promise4");
+//     }, 0);
+//   });
+// });
+
+// console.log("script end");
+//script start、a1 start、a2、promise2、script end、promise1、a1 end、promise2.then、promise3、setTimeout、promise4
