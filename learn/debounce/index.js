86d8c04e0b5199ef5a081c6edf892e73f3419d02
suchcl <ya5969@163.com>
2024-12-05 19:34:14 +0800

    防抖函数deo

--- learn/debounce/index.js
+++ learn/debounce/index.js
@@ -1,28 +1,20 @@
 let btn = document.querySelector("#btn");
 
 function handle(e){
-    console.log('%c [  ]-5', 'font-size:13px; background:pink; color:#bf2c9f;', "22222", this, e);
+    console.log('%c [  ]-5', 'font-size:13px; background:pink; color:#bf2c9f;', "333", this, e);
 }
 
 btn.addEventListener("click", debounce(handle));
 
-// function debounce(fn, delay = 1000){
-//     let timer = null;
-//     return function(e){
-//         if(timer){
-//             clearTimeout(timer);
-//         }
-//         timer = setTimeout(() => {
-//             fn.call(this, e); // 改变this指向，默认setTimeout中的this会指向window
-//         }, delay);
-//     }
-// }
-
-
 function debounce(fn, delay = 1000){
+    let timer = null;
     return function(e){
-        setTimeout(function(){
-            fn();
-        }, delay);
+        if(timer){
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            // 显示指定this，e用来接收其他事件参数
+            fn.call(this,e);
+        },delay);
     }
 }
