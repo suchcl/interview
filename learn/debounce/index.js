047861bc902a35fee913e8493bb50dc31771cc89
suchcl <ya5969@163.com>
2024-12-05 15:54:49 +0800

    demo

---
+++ learn/debounce/index.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", function(){
+    
+});
+
+function debounce(fn,delay){
+
+}
+
+const a = 10;
+var b = 20;
+
+function test(){
+    console.log('%c [  ]-14', 'font-size:13px; background:pink; color:#bf2c9f;', this.a, this, this.b)
+}
+// test();
