56e5179adf0c7797a1f14f7b886ef99027b22abc
suchcl <ya5969@163.com>
2024-12-06 16:00:58 +0800

    深拷贝

--- learn/functions/copy.js
+++ learn/functions/copy.js
@@ -1,19 +1,36 @@
-// json深拷贝
-const obj = {
-    a: 1,
-    b: {
-        c: 2
-    }
+const originalObj = {
+    name: "Alice",
+    age: 18,
+    date: new Date(),
+    map: new Map([["key", "value"]])
 };
-const newObj = JSON.stringify(obj);
-const targetObj = JSON.parse(newObj);
-targetObj.name = "Nicholas Zakas";
-console.log('%c [ targetObj ]-11', 'font-size:13px; background:pink; color:#bf2c9f;', targetObj)
 
-targetObj = {
-    a: 1,
-    b: {
-        c: 2
-    },
-    name: "Nicholas Zakas"
-};
+console.log('%c [ 原始对象 ]-2', 'font-size:13px; background:pink; color:#bf2c9f;', originalObj);
+
+const targetObj = structuredClone(originalObj); // 深拷贝
+targetObj.map.set("key2", 1888);
+
+console.log('%c [ 深拷贝对象 ]-11', 'font-size:13px; background:pink; color:#bf2c9f;', targetObj)
+
+
+function deepCopy(source, hash = new WeakMap()){
+    if(obj === null || typeof obj !== "object"){
+        return source;
+    }
+
+    if(hash.has(source)){
+        return hash.get(source);
+    }
+
+    let target = Array.isArray(source) ? [] : {};
+    Reflect.ownKeys(source).forEach(key => {
+        const val = source[key];
+        if(typeof val === "object" && val !== null){
+            target[key] = deepCopy(val, hash);
+        }else {
+            target[key] = val;
+        }
+    })
+
+    return target;
+}
