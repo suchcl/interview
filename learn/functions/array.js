3a41aeec55ddf6e8782889da799c7bfc9b22c9ae
suchcl <ya5969@163.com>
2024-12-06 19:38:50 +0800

    通过forEach遍历和includes来实现数组去重

--- learn/functions/array.js
+++ learn/functions/array.js
@@ -18,3 +18,16 @@ function removalDuplicate(arr){
 }
 removalDuplicate(arr2)
 console.log('%c [ 通过for循环和indexOf来实现去重 (arr2) ]-20', 'font-size:13px; background:pink; color:#bf2c9f;', removalDuplicate(arr2))
+
+const arr3 = [2, 3, 1, 2, 2, 3, 4, 4, 5];
+
+function removeDup(arr){
+    const targetArr = [];
+    arr.forEach(item => {
+        if(!targetArr.includes(item)){
+            targetArr.push(item);
+        }
+    });
+    return targetArr;
+}
+console.log('%c [ 通过forEach和includes实现数组去重 (arr3) ]-34', 'font-size:13px; background:pink; color:#bf2c9f;', removeDup(arr3))
