e517236f63a873fd3d4f55e51ed24717bee00edd
suchcl <ya5969@163.com>
2024-12-21 21:07:53 +0800

    走通了vue的案例

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-        test    
+        <h3>{{ pageTitle }}</h3>
+        <ul>
+            <li v-for="item in data" key="{item.id}">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
 
-<script setup lang="ts">
-
+<script lang="ts">
+import { ref } from 'vue';
+import { data } from './testData';
+export default {
+    setup() {
+        const pageTitle = ref("功能测试页面");
+        
+        return {
+            pageTitle,
+            data
+        };
+    }
+}
 </script>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
---
+++ src/pages/test/testData.ts
@@ -0,0 +1,14 @@
+const data = [
+    {
+        id: 1,
+        name: "张三",
+        age: 16
+    },
+    {
+        id: 2,
+        name: "李四",
+        age: 22
+    }
+];
+
+export { data };
