fcb2761b1c6bc80b0e6431c7b0f698649435f2cd
suchcl <ya5969@163.com>
2024-12-22 17:14:28 +0800

    v-model

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -2,6 +2,23 @@
     <div>
         <h3>list 列表页面</h3>
         <img class="pic" :src="imgSrc" :alt="imgAlt">
+        <form @submit.prevent="handleSubmit">
+            <ul class="ul-list">
+                <li>
+                    <label for="username">姓名:</label>
+                    <input type="text" id="username" v-model="username">
+                </li>
+                <li>
+                    <label for="age">年龄:</label>
+                    <input type="text" id="age" v-model="age">
+                </li>
+            </ul>
+            <button type="submit">提交</button>
+        </form>
+        <ul class="user-info">
+            <li>姓名: {{ username }}</li>
+            <li>年龄: {{ age }}</li>
+        </ul>
     </div>
 </template>
 
@@ -10,10 +27,23 @@
     export default{
         setup(){
             const imgSrc=ref("/images/i1.png");
-            const imgAlt = ref("我的自定义alt");
+            const imgAlt = ref("我的自定义alt")
+            const username = ref("");
+            const age = ref("");
+            
+            const handleSubmit = () => {
+                const user = {
+                    username: username.value,
+                    age: age.value
+                };
+                console.log('%c [ user ]-36', 'font-size:13px; background:pink; color:#bf2c9f;', user);
+            }
             return {
                 imgSrc,
-                imgAlt
+                imgAlt,
+                username,
+                age,
+                handleSubmit
             };
         }
     }
