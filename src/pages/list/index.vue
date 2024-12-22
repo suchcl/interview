16dfdeaee659dd4c693c18caf30ba7ad8271dcd3
suchcl <ya5969@163.com>
2024-12-22 12:33:01 +0800

    bind指令

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
-        <h3>list 列表页面</h3>   
+        <h3>list 列表页面</h3>
+        <img class="pic" :src="imgSrc" :alt="imgAlt">
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+    import { ref } from 'vue';
+    export default{
+        setup(){
+            const imgSrc=ref("/images/i1.png");
+            const imgAlt = ref("我的自定义alt");
+            return {
+                imgSrc,
+                imgAlt
+            };
+        }
+    }
 </script>
 
 <style scoped>
+.pic {
+    width: 300px;
+    height: 400px;
+}
 </style>
