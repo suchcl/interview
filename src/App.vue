06e6de5fcba4ad3b11a763b7d2e0b43c63b5e4b3
suchcl <ya5969@163.com>
2024-12-25 15:13:59 +0800

    抽离header组件

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,33 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <ul class="menu">
-    <li>
-      <RouterLink to="/home">首页</RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/list">列表页</RouterLink>
-    </li>
-    <li>
-      <router-link to="/test">功能测试页面</router-link>
-    </li>
-  </ul>
+  <Header />
   <RouterView />
   <h3 class="initial">这是一个初始页面</h3>
 </template>
-
+<script setup lang="ts">
+import Header from "./components/header/index.vue"
+</script>
 <style scoped>
 .menu {
   display: flex;
   justify-content: center;
   align-items: center;
+
   li {
     margin: 0 15px;
+
     a {
       color: #333;
-      &:hover{
+
+      &:hover {
         color: #f20;
       }
-      &.router-link-active{
+
+      &.router-link-active {
         color: #f20;
       }
     }
   }
 }
-.initial {
-  
-}
+
+.initial {}
 </style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="menu">
+        <ul class="nav">
+            <li>
+                <RouterLink to="/home">首页</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/list">列表页</RouterLink>
+            </li>
+            <li>
+                <router-link to="/test">功能测试页面</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup() {
+        return {
+        };
+    }
+}
+</script>
