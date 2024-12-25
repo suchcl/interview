0fd184cb3a615223874cabc74385c9c96dfd439e
suchcl <ya5969@163.com>
2024-12-25 21:55:34 +0800

    简单样式调整

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -5,15 +5,26 @@
             <RouterLink to="/list">列表页</RouterLink>
             <router-link to="/test">功能测试页面</router-link>
         </div>
+        <div class="user-center">
+            <p>{{ message }}</p>
+            <!-- <Timer :message="message" /> -->
+        </div>
     </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import Timer from "./timer.vue"
 export default {
     name: 'Header',
+    components: {
+        Timer
+    },
     setup() {
+        const message = ref("欢迎光临!");
+        console.log('%c [ message ]-22', 'font-size:13px; background:pink; color:#bf2c9f;', message);
         return {
+            message
         };
     }
 }
@@ -21,6 +32,10 @@ export default {
 
 <style>
 .menu {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
     height: 100%;
 
     .nav {
@@ -42,6 +57,9 @@ export default {
                 background-color: #1677ff;
             }
         }
+    }
+    .user-center {
+        padding-right: 20px;
     }
 }
 </style>
