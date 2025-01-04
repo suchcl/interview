6e6d71aa54d1d5f8e584536d4c4174dc10379f6b
suchcl <ya5969@163.com>
2025-01-04 16:49:21 +0800

    vue3 静态提升 预字符串化

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h3>Detail</h3>
+        
     </div>
 </template>
 
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="home">
-        <h3>首页</h3>
+        <h3>首页</h3> <!-- 静态提升 -->
+        {{ message }}
+        {{ concatStr }}
     </div>
 </template>
 
 <script setup lang="ts">
-    console.log('%c [  ]-9', 'font-size:13px; background:pink; color:#bf2c9f;', "00000000");
+// <h3>首页</h3>是一个静态节点，vue3会在编译的时候识别并提升它，避免在每次渲染时都重新创建
+import { ref } from 'vue';
+const message = ref('Hello Vue!');
+
+const str1 = "Hello";
+const str2 = "World";
+const concatStr = `${str1},${str2}`; // 预字符串化
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
