051a9d636c5b0b4e94560d1c9a0016aef6325517
suchcl <ya5969@163.com>
2025-03-24 08:56:00 +0800

    组件通信

---
+++ src/pages/notes/components/Button/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button>按钮子组件</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Button",
+    setup() {
+        const buttonClick = () => {
+            console.log("按钮子组件");
+        }
+        return {
+            buttonClick,
+            message: "按钮子组件"
+        }
+    }
+}
+</script>
--- src/pages/notes/index.vue
+++ src/pages/notes/index.vue
@@ -2,25 +2,41 @@
     <div class="noteList">
         <div class="title">笔记列表</div>
         <NoteItem @goDetail="handleGoDetail" />
+        <Button ref="btnRef" />
+        <button @click="handleClick">父组件中的按钮</button>
     </div>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import NoteItem from "./components/NoteItem/index.vue";
-    export default {
-        components: {
-            NoteItem
-        },
-        setup() {
-            const handleGoDetail = (msg: string) => {
-                console.log(msg);
-            };
-            return {
-                message: "笔记列表",
-                handleGoDetail
+import Button from "./components/Button/index.vue";
+
+export default {
+    components: {
+        NoteItem,
+        Button
+    },
+    setup() {
+        const btnRef = ref(null);
+        const handleGoDetail = (msg: string) => {
+            console.log(msg);
+        };
+        const handleClick = () => {
+            console.log('%c [  ]-26', 'font-size:13px; background:pink; color:#bf2c9f;', "0000000");
+            if(btnRef.value){
+                btnRef.value.buttonClick();
+            }else {
+                console.log('%c [  ]-31', 'font-size:13px; background:pink; color:#bf2c9f;', "子组件尚未被使用");
             }
         }
-    }
+        return {
+            message: "笔记列表",
+            handleGoDetail,
+            handleClick
+        };
+    },
+};
 </script>
 
 <style>
